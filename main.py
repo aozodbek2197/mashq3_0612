5000b06ae42e8ad05f4bf6228698d5bc60fa7719
aozodbek2197 <aozodbek2197@gmail.com>
2025-12-06 15:55:34 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# # 3-misol
+# roy = [4, 7, 2, 5, 1, 10]
+# natija = []
+
+# for i in range(len(roy)):
+#     natija.append(i * roy[i])
+
+# print(natija)
+# # 4-misol
+# words = ["dasturlash", "kitob", "shunday", "kompyuter", "ilm", "maktab"]
+
+# sorted_by_len = sorted(words, key=len, reverse=True)
+# print(sorted_by_len[0])
+# print(sorted_by_len[1])
